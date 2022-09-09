dd93247ddf9c639d797c06b73aed5f15806d7612
Kenny Wai <97656734+KennyWCoder@users.noreply.github.com>
2022-09-08 19:37:04 -0700

    comments added

--- scores.js
+++ scores.js
@@ -2,8 +2,10 @@ var highscores = JSON.parse(localStorage.getItem("highscores")) || [];
 var clearScores = document.getElementById("clear-button");
 var scorePrinting = document.querySelector(".score-list");
 
+//when window load, print highs scores
 window.addEventListener("load", function(){printHighScores()});
 
+//function to create the list of scores and sort function added
 function printHighScores() {
     for (var i = 0; i < highscores.length; i++) {
         highscores = sorting(highscores, "score");
@@ -14,6 +16,7 @@ function printHighScores() {
     }
 }
 
+//sort the highscores list
 function sorting(array, key) {
     return array.sort(function(a,b) {
         if (a.timerCount < b.timerCount) {
@@ -22,7 +25,7 @@ function sorting(array, key) {
         return -1;
     })
 }
-
+//clear highscore
 clearScores.addEventListener("click", function() {
     localStorage.removeItem("highscores");
     window.location.reload();
